e5888e50e87ee2917e1c5440880e49dc21cd19f7
ZerGio23 <sergio.apaza1432@gmail.com>
2025-12-26 23:54:07 -0400

    fix: users not in swagger

--- backend-nest/src/users/users.controller.ts
+++ backend-nest/src/users/users.controller.ts
@@ -1,13 +1,5 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) { }
-
-  @Post('crear-usuario')
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
 }
